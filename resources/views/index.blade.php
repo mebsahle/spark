c7f696582097abd07cacb4ebd71bc9c1f1e31100
Tekle <tekleyitayew12@gmail.com>
2020-10-08 11:53:15 -0700

    react js added

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="style/custom.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<body>
+    <div id="app">
+        <div class="container">
+         <h2>
+             <strong class="spark__logo">
+                 <i class="fa fa-bolt" aria-hidden="true"></i>
+             </strong>park Dating App
+
+        </h2>
+        </div>
+    </div>
+    <script src="js/app.js"></script>
+</body>
+</html>
